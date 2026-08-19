4354f18c8ee93f3f8aef6074898ce739a2538ba6
semihselvi <semihselvi@hotmail.com>
2026-08-19 13:18:02 +0300

    Add world radar configuration

---
+++ config.py
@@ -0,0 +1,80 @@
+import os
+
+COLLECTION = os.getenv("WORLD_FIRESTORE_COLLECTION", "bay_s_world_leads")
+SCAN_LOG_COLLECTION = os.getenv("WORLD_FIRESTORE_SCAN_COLLECTION", "bay_s_world_scans")
+LOOKBACK_HOURS = int(os.getenv("WORLD_LOOKBACK_HOURS", "24"))
+MAX_RESULTS_PER_SOURCE = int(os.getenv("WORLD_MAX_RESULTS_PER_SOURCE", "15"))
+EXA_NUM_RESULTS = int(os.getenv("WORLD_EXA_NUM_RESULTS", "8"))
+
+MARKETS = {
+    "north_cyprus": ["North Cyprus", "Northern Cyprus", "Kuzey Kıbrıs", "Северный Кипр", "TRNC", "Iskele", "İskele", "Long Beach", "Kyrenia", "Girne", "Esentepe", "Lapta", "Yeniboğaziçi", "Famagusta", "Gazimağusa", "Tatlısu", "Bafra", "Boğaz", "Dipkarpaz"],
+    "turkey": ["Turkey", "Türkiye", "Antalya", "Alanya", "Mersin", "Istanbul", "İstanbul", "Izmir", "İzmir", "Bodrum", "Fethiye", "Muğla"],
+    "montenegro": ["Montenegro", "Budva", "Kotor", "Tivat", "Podgorica", "Bar", "Herceg Novi", "Karadağ", "Черногория"],
+    "greece": ["Greece", "Athens", "Thessaloniki", "Crete", "Rhodes", "Corfu", "Piraeus", "Greek Golden Visa"],
+    "portugal": ["Portugal", "Lisbon", "Porto", "Algarve", "Madeira", "Portugal Golden Visa"],
+    "spain": ["Spain", "Madrid", "Barcelona", "Malaga", "Alicante", "Valencia", "Marbella"],
+    "italy": ["Italy", "Rome", "Milan", "Sicily", "Puglia", "Tuscany", "Italian property"],
+    "cyprus": ["Cyprus", "Republic of Cyprus", "Paphos", "Limassol", "Larnaca", "Nicosia"],
+    "germany": ["Germany", "Deutschland", "Berlin", "Munich", "Frankfurt", "Hamburg", "Cologne", "Köln"],
+    "netherlands": ["Netherlands", "Amsterdam", "Rotterdam", "The Hague", "Utrecht"],
+    "belgium": ["Belgium", "Brussels", "Antwerp", "Ghent"],
+    "france": ["France", "Paris", "Nice", "Cannes", "Marseille", "Lyon"],
+    "lithuania": ["Lithuania", "Vilnius", "Kaunas", "Klaipeda"],
+    "russia": ["Russia", "Россия", "Москва", "Санкт-Петербург"],
+    "kazakhstan": ["Kazakhstan", "Казахстан", "Almaty", "Алматы", "Astana", "Астана"],
+    "uk": ["United Kingdom", "UK", "England", "London", "Manchester", "Birmingham", "Liverpool", "Leeds", "Brighton"],
+    "poland": ["Poland", "Warsaw", "Krakow", "Gdansk", "Wroclaw"],
+    "czech_republic": ["Czech Republic", "Czechia", "Prague", "Brno"],
+    "austria": ["Austria", "Vienna", "Salzburg", "Innsbruck"],
+}
+
+INTENT_PHRASES = [
+    "looking to buy", "want to buy", "planning to buy", "ready to buy", "looking for property",
+    "looking for an apartment", "looking for a house", "looking for a villa", "property wanted",
+    "where should I buy", "which area should I buy", "buying property", "investment property",
+    "property investment", "cash buyer", "property budget", "moving to", "relocating to",
+    "viewing property", "property viewing", "make an offer", "mortgage", "deposit", "lawyer",
+    "title deed", "payment plan", "due diligence", "golden visa", "residency by investment",
+    "property for residency", "second home", "holiday home", "retirement home", "ev almak istiyorum",
+    "daire almak istiyorum", "satın almak istiyorum", "ev arıyorum", "daire arıyorum", "villa arıyorum",
+    "arsa arıyorum", "yatırım için ev", "Kıbrıs'a taşınmak", "hangi bölgede ev alınır", "бюджет",
+    "хочу купить", "хотим купить", "ищу квартиру", "ищу апартамент", "ищу виллу", "ищу дом",
+    "ищу недвижимость", "куплю квартиру", "куплю недвижимость", "готов купить", "планирую купить",
+    "недвижимость за рубежом", "инвестиции в недвижимость", "переезд", "переезжаем", "ВНЖ",
+    "haus kaufen", "wohnung kaufen", "immobilie kaufen", "acheter une maison", "acheter un appartement",
+    "acheter un bien immobilier", "huis kopen", "woning kopen", "vastgoed kopen", "αγορά ακινήτου",
+]
+
+EXCLUDE_PHRASES = [
+    "contact us", "call us", "whatsapp us", "dm for details", "our properties", "our projects",
+    "property developer", "real estate agency", "estate agent", "realtor", "broker", "listing page",
+    "available units", "new project", "developer", "we sell", "commission", "property portal",
+    "for rent", "kiralık", "сдам", "сдается", "продам", "продается", "агентство", "застройщик", "риэлтор",
+]
+
+NEGATIVE_PHRASES = [
+    "already bought", "we bought", "i bought", "already purchased", "purchase completed", "not buying",
+    "no longer looking", "renting instead", "satın aldım", "aldık", "almaktan vazgeç", "купил", "купили", "передумал",
+]
+
+ROUTES = {
+    "north_cyprus": "Prime Kıbrıs",
+    "turkey": "Turkey Partner",
+    "greece": "Golden Visa Partner",
+    "portugal": "Golden Visa Partner",
+    "cyprus": "Golden Visa Partner",
+    "germany": "Germany Partner",
+    "netherlands": "Netherlands Partner",
+    "france": "France Partner",
+    "montenegro": "Partner Network",
+    "uk": "Partner Network",
+    "belgium": "Partner Network",
+    "lithuania": "Partner Network",
+    "italy": "Partner Network",
+    "spain": "Partner Network",
+    "russia": "Partner Network",
+    "kazakhstan": "Partner Network",
+    "poland": "Partner Network",
+    "czech_republic": "Partner Network",
+    "austria": "Partner Network",
+}
